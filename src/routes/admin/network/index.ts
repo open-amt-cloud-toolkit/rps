9b0a72089e3e72d8ff1f46293731adf4fba4ba08
madhavi-losetty <madhavi.losetty@intel.com>
2021-01-21 11:58:59 -0800

    fix(networkConfig): removed delete and edit api's

--- src/routes/admin/network/index.ts
+++ src/routes/admin/network/index.ts
@@ -3,19 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  * Author : Ramu Bachala
  **********************************************************************/
-import { deleteNetProfile } from './deleteNetProfile'
-
 import { Router } from 'express'
 import { allProfiles } from './all'
 import { getNetProfile } from './getProfile'
 import { createNetProfile } from './createNetProfile'
-import { editNetProfile } from './editNetProfile'
 const profileRouter: Router = Router()
 
 profileRouter.get('/', allProfiles)
 profileRouter.get('/:profileName', getNetProfile)
 profileRouter.post('/create', createNetProfile)
-profileRouter.patch('/edit', editNetProfile)
-profileRouter.delete('/:profileName', deleteNetProfile)
 
 export default profileRouter
