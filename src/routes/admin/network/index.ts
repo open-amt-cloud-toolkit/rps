4a260dbed9acf0fd8d391343f0cc1bfa8ea2c330
madhavi-losetty <madhavi.losetty@intel.com>
2021-02-22 11:01:28 -0800

    test(api): added unit tests to update amt profiles

--- src/routes/admin/network/index.ts
+++ src/routes/admin/network/index.ts
@@ -7,16 +7,16 @@ import { Router } from 'express'
 import { allProfiles } from './all'
 import { getNetProfile } from './getProfile'
 import { createNetProfile } from './createNetProfile'
-import { editNetProfile } from './editNetProfile'
+// import { editNetProfile } from './editNetProfile'
 import { networkValidator } from './networkValidator'
-import { deleteNetProfile } from './deleteNetProfile'
+// import { deleteNetProfile } from './deleteNetProfile'
 
 const profileRouter: Router = Router()
 
 profileRouter.get('/', allProfiles)
 profileRouter.get('/:profileName', getNetProfile)
 profileRouter.post('/', networkValidator(), createNetProfile)
-profileRouter.patch('/', networkValidator(), editNetProfile)
-profileRouter.delete('/:profileName', deleteNetProfile)
+// profileRouter.patch('/', networkValidator(), editNetProfile)
+// profileRouter.delete('/:profileName', deleteNetProfile)
 
 export default profileRouter
