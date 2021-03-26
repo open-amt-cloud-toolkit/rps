eb16bddbad3ee34c9a02e75b6d5f9a73984aa98d
madhavi-losetty <madhavi.losetty@intel.com>
2021-03-26 15:29:54 -0700

    fix(cira): updated to accept ciraconfig name to case insensitive

--- src/db/CiraConfigFileStorageDb.ts
+++ src/db/CiraConfigFileStorageDb.ts
@@ -37,7 +37,7 @@ export class CiraConfigFileStorageDb implements ICiraConfigDb {
    */
   async getCiraConfigByName (ciraConfigName: string): Promise<CIRAConfig> {
     this.logger.debug(`getCiraConfigByName: ${ciraConfigName}`)
-    const ciraConfig: CIRAConfig = this.ciraConfigs.find(item => item.configName === ciraConfigName) || null
+    const ciraConfig: CIRAConfig = this.ciraConfigs.find(item => item.configName.toLowerCase() === ciraConfigName.toLowerCase()) || null
     return ciraConfig
   }
 
@@ -54,8 +54,8 @@ export class CiraConfigFileStorageDb implements ICiraConfigDb {
     let found: boolean = false
     for (let i = 0; i < this.ciraConfigs.length; i++) {
       this.logger.silly(`Checking ${this.ciraConfigs[i].configName}`)
-      if (this.ciraConfigs[i].configName === ciraConfigName) {
-        const profileUsingThisConfig = this.amtProfiles.find(profile => profile.ciraConfigName === ciraConfigName)
+      if (this.ciraConfigs[i].configName.toLowerCase() === ciraConfigName.toLowerCase()) {
+        const profileUsingThisConfig = this.amtProfiles.find(profile => profile.ciraConfigName.toLowerCase() === ciraConfigName.toLowerCase())
         if (typeof profileUsingThisConfig !== 'undefined') {
           this.logger.error('Cannot delete the CIRA config. An AMT Profile is already using it.')
           throw new RPSError(CIRA_CONFIG_DELETION_FAILED_CONSTRAINT(ciraConfigName), 'Foreign key violation')
@@ -81,7 +81,7 @@ export class CiraConfigFileStorageDb implements ICiraConfigDb {
    * @returns {CIRAConfig} Returns created cira config object
    */
   async insertCiraConfig (ciraConfig: CIRAConfig): Promise<CIRAConfig> {
-    if (this.ciraConfigs.some(item => item.configName === ciraConfig.configName)) {
+    if (this.ciraConfigs.some(item => item.configName.toLowerCase() === ciraConfig.configName.toLowerCase())) {
       this.logger.info(`Cira Config already exists: ${ciraConfig.configName}`)
       throw new RPSError(CIRA_CONFIG_INSERTION_FAILED_DUPLICATE(ciraConfig.configName), 'Unique key violation')
     } else {
@@ -100,10 +100,12 @@ export class CiraConfigFileStorageDb implements ICiraConfigDb {
    */
   async updateCiraConfig (ciraConfig: CIRAConfig): Promise<CIRAConfig> {
     this.logger.debug(`update CiraConfig: ${ciraConfig.configName}`)
-    const isMatch = (item): boolean => item.configName === ciraConfig.configName
+    const isMatch = (item): boolean => item.configName.toLowerCase() === ciraConfig.configName.toLowerCase()
     const index = this.ciraConfigs.findIndex(isMatch)
     if (index >= 0) {
+      const oldConfig = await this.getCiraConfigByName(ciraConfig.configName)
       this.ciraConfigs.splice(index, 1)
+      ciraConfig.configName = oldConfig.configName
       this.ciraConfigs.push(ciraConfig)
       this.updateConfigFile()
       this.logger.info(`Cira Config updated: ${ciraConfig.configName}`)
