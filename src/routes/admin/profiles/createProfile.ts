93803a8eb49218a04a67d67f1775630da4387402
Mike <mjohanson@gesdate.com>
2021-09-03 22:56:22 -0700

    refactor(database): simplify database organization

--- src/routes/admin/profiles/createProfile.ts
+++ src/routes/admin/profiles/createProfile.ts
@@ -1,10 +1,7 @@
 /*********************************************************************
- * Copyright (c) Intel Corporation 2019
+ * Copyright (c) Intel Corporation 2021
  * SPDX-License-Identifier: Apache-2.0
- * Author : Ramu Bachala
  **********************************************************************/
-import { IProfilesDb } from '../../../interfaces/database/IProfilesDb'
-import { ProfilesDbFactory } from '../../../repositories/factories/ProfilesDbFactory'
 import { EnvReader } from '../../../utils/EnvReader'
 import Logger from '../../../Logger'
 import { API_RESPONSE, API_UNEXPECTED_EXCEPTION } from '../../../utils/constants'
@@ -14,26 +11,23 @@ import { MqttProvider } from '../../../utils/MqttProvider'
 import { Request, Response } from 'express'
 
 export async function createProfile (req: Request, res: Response): Promise<void> {
-  let profilesDb: IProfilesDb = null
   const log = new Logger('createProfile')
   const amtConfig: AMTConfiguration = req.body
   amtConfig.tenantId = req.tenantId
   try {
-    profilesDb = ProfilesDbFactory.getProfilesDb()
     const pwdBefore = amtConfig.amtPassword
     const mebxPwdBefore = amtConfig.mebxPassword
     if (req.secretsManager) {
       amtConfig.amtPassword = 'AMT_PASSWORD'
       amtConfig.mebxPassword = 'MEBX_PASSWORD'
     }
-    const results: AMTConfiguration = await profilesDb.insert(amtConfig)
+    const results: AMTConfiguration = await req.db.profiles.insert(amtConfig)
     if (results != null) {
       // profile inserted  into db successfully.
       if (req.secretsManager) {
         // store the passwords in Vault
-        const data = { data: { AMT_PASSWORD: '', MEBX_PASSWORD: '' } }
-        data.data.AMT_PASSWORD = pwdBefore
-        data.data.MEBX_PASSWORD = mebxPwdBefore
+        const data = { data: { AMT_PASSWORD: pwdBefore, MEBX_PASSWORD: mebxPwdBefore } }
+
         log.debug('AMT and MEBX Passwords written to vault')
         await req.secretsManager.writeSecretWithObject(`${EnvReader.GlobalEnvConfig.VaultConfig.SecretsPath}profiles/${amtConfig.profileName}`, data)
       }
