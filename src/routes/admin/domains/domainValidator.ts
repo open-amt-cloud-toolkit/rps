b00cf1c3fac3eb94c8a88ec8a04b19f94d978874
Mike <mjohanson@gesdate.com>
2021-11-03 09:31:08 -0700

    refactor(api): remove password validation from domain cert upload
    closes #456 AB#4231

--- src/routes/admin/domains/domainValidator.ts
+++ src/routes/admin/domains/domainValidator.ts
@@ -31,8 +31,8 @@ export const domainInsertValidator = (): any => {
       .not()
       .isEmpty()
       .withMessage('Provisioning Cert Password is required')
-      .matches('^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9$@$!%*#?&-_~^]{8,32}$')
-      .withMessage('Password should contain at least one lowercase letter, one uppercase letter, one numeric digit,and one special character and password length should be in between 8 to 32.')
+      .isLength({ max: 64 })
+      .withMessage('Password should not exceed 64 characters in length')
   ]
 }
 
@@ -54,7 +54,7 @@ export const domainUpdateValidator = (): any => {
       .withMessage('Provisioning Cert Storage Format is either "raw" or "string"'),
     check('provisioningCertPassword')
       .optional()
-      .matches('^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9$@$!%*#?&-_~^]{8,32}$')
-      .withMessage('Password is required field should contains at least one lowercase letter, one uppercase letter, one numeric digit,and one special character and password length should be in between 8 to 32.')
+      .isLength({ max: 64 })
+      .withMessage('Password should not exceed 64 characters in length')
   ]
 }
