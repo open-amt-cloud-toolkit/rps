188feadc80d03db73ad417b58dff04a4b761cfdd
Mike <michael.johanson@intel.com>
2022-03-16 13:14:56 -0700

    test(routes): increase coverage of "all" api calls

--- src/routes/admin/domains/all.test.ts
+++ src/routes/admin/domains/all.test.ts
@@ -4,19 +4,41 @@ import { getAllDomains } from './all'
 describe('Domains - All', () => {
   let resSpy
   let req
+  let getSpy: jest.SpyInstance
   beforeEach(() => {
     resSpy = createSpyObj('Response', ['status', 'json', 'end', 'send'])
     req = {
       db: { domains: { get: jest.fn() } },
       query: { }
     }
-    jest.spyOn(req.db.domains, 'get').mockResolvedValue([])
+    getSpy = jest.spyOn(req.db.domains, 'get').mockResolvedValue([])
     resSpy.status.mockReturnThis()
     resSpy.json.mockReturnThis()
     resSpy.send.mockReturnThis()
   })
   it('should get all', async () => {
     await getAllDomains(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
     expect(resSpy.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should get all with req.query.$count as true', async () => {
+    req.db.domains.getCount = jest.fn().mockImplementation().mockResolvedValue(123)
+    req.query.$count = true
+    await getAllDomains(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
+    expect(req.db.domains.getCount).toHaveBeenCalled()
+    expect(resSpy.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should set status to 500 if error occurs', async () => {
+    req.db.domains.getCount = jest.fn().mockImplementation(() => {
+      throw new TypeError('fake error')
+    })
+    req.query.$count = true
+    await getAllDomains(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
+    expect(req.db.domains.getCount).toHaveBeenCalled()
+    expect(resSpy.status).toHaveBeenCalledWith(500)
   })
 })
--- src/routes/admin/wireless/all.test.ts
+++ src/routes/admin/wireless/all.test.ts
@@ -4,19 +4,51 @@ import { allProfiles } from './all'
 describe('Wireless - All', () => {
   let resSpy
   let req
+  let getSpy: jest.SpyInstance
   beforeEach(() => {
     resSpy = createSpyObj('Response', ['status', 'json', 'end', 'send'])
     req = {
       db: { wirelessProfiles: { get: jest.fn() } },
       query: { }
     }
-    jest.spyOn(req.db.wirelessProfiles, 'get').mockResolvedValue([])
+    getSpy = jest.spyOn(req.db.wirelessProfiles, 'get').mockResolvedValue([])
     resSpy.status.mockReturnThis()
     resSpy.json.mockReturnThis()
     resSpy.send.mockReturnThis()
   })
+
   it('should get all', async () => {
     await allProfiles(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
     expect(resSpy.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should get all with wirelessConfigs length > 0', async () => {
+    req.db.wirelessProfiles.getCount = jest.fn().mockImplementation().mockResolvedValue(123)
+    req.query.$count = true
+    jest.spyOn(req.db.wirelessProfiles, 'get').mockResolvedValue(['abc'])
+    await allProfiles(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
+    expect(resSpy.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should get all with req.query.$count as true', async () => {
+    req.db.wirelessProfiles.getCount = jest.fn().mockImplementation().mockResolvedValue(123)
+    req.query.$count = true
+    await allProfiles(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
+    expect(req.db.wirelessProfiles.getCount).toHaveBeenCalled()
+    expect(resSpy.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should set status to 500 if error occurs', async () => {
+    req.db.wirelessProfiles.getCount = jest.fn().mockImplementation(() => {
+      throw new TypeError('fake error')
+    })
+    req.query.$count = true
+    await allProfiles(req, resSpy)
+    expect(getSpy).toHaveBeenCalled()
+    expect(req.db.wirelessProfiles.getCount).toHaveBeenCalled()
+    expect(resSpy.status).toHaveBeenCalledWith(500)
   })
 })
