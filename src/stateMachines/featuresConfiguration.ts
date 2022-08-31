9b5114bca746e61a43db112a394e3760593789bb
Tim Shockley <timothy.shockley@intel.com>
2022-08-31 12:23:50 -0700

    fix(profile features): corrected CIM_KVMRedirection enum comparison

--- src/stateMachines/featuresConfiguration.ts
+++ src/stateMachines/featuresConfiguration.ts
@@ -185,8 +185,8 @@ export class FeaturesConfiguration {
           solEnabled = true
         }
         const kvmEnabled = (
-          context.CIM_KVMRedirectionSAP.EnabledState === Common.Models.AMT_REDIRECTION_SERVICE_ENABLE_STATE.Enabled ||
-          context.CIM_KVMRedirectionSAP.EnabledState === Common.Models.AMT_REDIRECTION_SERVICE_ENABLE_STATE.EnabledButOffline
+          context.CIM_KVMRedirectionSAP.EnabledState === Common.Models.CIM_KVM_REDIRECTION_SAP_ENABLED_STATE.Enabled ||
+          context.CIM_KVMRedirectionSAP.EnabledState === Common.Models.CIM_KVM_REDIRECTION_SAP_ENABLED_STATE.EnabledButOffline
         )
         let isRedirectionChanged = false
 
