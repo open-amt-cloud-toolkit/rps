54d0b15b58db16af0471e6eb5ec965c254be153a
Tim Shockley <timothy.shockley@intel.com>
2022-11-14 16:14:27 -0700

    fix(factory): db singleton was not actually added missing unit test for vault factory

--- src/repositories/factories/DbCreatorFactory.ts
+++ src/repositories/factories/DbCreatorFactory.ts
@@ -2,20 +2,19 @@ import { IDB } from '../../interfaces/database/IDb'
 import { RPSConfig } from '../../models'
 
 export class DbCreatorFactory {
-  static instance: IDB
+  private static instance: IDB
   config: RPSConfig
+
   constructor (config: RPSConfig) {
     this.config = config
   }
 
   async getDb (): Promise<IDB> {
-    const provider = await import(`../../data/${this.config.dbProvider}`)
-
     if (DbCreatorFactory.instance == null) {
-      // eslint-disable-next-line new-cap
-      return new provider.default(this.config.connectionString)
-    } else {
-      return DbCreatorFactory.instance
+      const { default: Provider }: { default: new (connectionString: string) => IDB } =
+        await import(`../../data/${this.config.dbProvider}`)
+      DbCreatorFactory.instance = new Provider(this.config.connectionString)
     }
+    return DbCreatorFactory.instance
   }
 }
