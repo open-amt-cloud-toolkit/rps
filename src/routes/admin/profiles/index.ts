4998c3f31e0692ca3454606918dcda6d260796ad
Walt M <walter.a.morton@intel.com>
2023-04-20 11:02:47 -0700

    remove mistake

--- src/routes/admin/profiles/index.ts
+++ src/routes/admin/profiles/index.ts
@@ -20,6 +20,6 @@ profileRouter.get('/', odataValidator(), validateMiddleware, allProfiles)
 profileRouter.get('/:profileName', getProfile)
 profileRouter.post('/', amtProfileValidator(), validateMiddleware, createProfile)
 profileRouter.patch('/', profileUpdateValidator(), validateMiddleware, ifMatchMiddleware, editProfile)
-profileRouter.delete('/:profileName', validateMiddleware, deleteProfile)
+profileRouter.delete('/:profileName', deleteProfile)
 
 export default profileRouter
